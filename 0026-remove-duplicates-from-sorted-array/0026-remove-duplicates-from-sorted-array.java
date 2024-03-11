955140540f64bec0b67420605265aa81072d5491
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2024-03-11 21:06:22 +0530

    Time: 0 ms (100.00%), Space: 44.8 MB (42.89%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         
-        int rd=0;
+        int rd=0; //we have used rd for storing the arr which is not repeating
         for(int i=0;i<nums.length;i++){
             
             if(nums[rd]!=nums[i]){
