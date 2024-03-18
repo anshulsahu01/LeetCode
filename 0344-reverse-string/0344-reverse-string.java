01b49a07ed401ef0c157a57cfabff527b0b7ce55
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2024-03-18 10:54:45 +0530

    Time: 74 ms (5.28%), Space: 46.1 MB (83.93%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void reverseString(char[] s) {
+       
+        String str = "";                   //Allocate extra space
+        
+        for(int i=s.length-1; i>=0; i--)   /*Add to extra space from rear to front */
+            str += s[i];
+        
+        for(int i=0; i<s.length; i++)      /*Set reversed 'str' into char array 's' */
+            s[i] = str.charAt(i);
+    }
+        
+        
+
+        
+    
+}
