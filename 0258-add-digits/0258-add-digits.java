1f9df497aeba7f3fdb6ea4374f31b7ae20523d7d
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2024-03-27 21:35:46 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (69.13%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int addDigits(int num) {
+        
+         int rem;
+        if(num ==0){
+            return num ;
+            
+        }
+        else if(num%9==0){
+            return 9;
+        }
+        else{
+            rem = num%9;
+        }
+        return rem;
+    }
+}
