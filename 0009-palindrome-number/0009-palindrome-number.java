bb6f76c7e0de8dbef60921c6b09b5b88dda9aed5
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2024-05-07 11:18:21 +0530

    Time: 4 ms (100.00%), Space: 44.1 MB (35.38%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,5 +1,11 @@
 class Solution {
     public boolean isPalindrome(int x) {
+  
+        //we have taken another variable so that the value of x can be stored in that it will be used for checking inn last 
+        
+        // and we have taken a condition if  x is negative so it can't be P so in starting we can say it is false
+        
+        
         int t= x;
         int rev=0;
         if(x<0){
