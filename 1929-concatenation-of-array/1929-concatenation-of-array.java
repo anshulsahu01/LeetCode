6337c798d3d8462c005aa60e4f77bffac078c2f7
Anshul Sahu <113385391+anshulsahu01@users.noreply.github.com>
2024-05-15 11:23:18 +0530

    Time: 1 ms (95.93%), Space: 44.9 MB (83.28%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.java
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -1,12 +1,18 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
         int size=2*nums.length;
+        
+        //ye size hum ne isliye lagay hai quki humko ek arry chahiye double size ka
         int n=nums.length;
+        
         int[] ans =new int[size];
+        
+        // ans ke first 3 index mai value put kr ray hai
         
         for(int i=0;i<n;i++){
             ans[i]=nums[i];
         }
+        //ans ke 3index ke baad value daal ray hai
         
         for(int i=0;i<n;i++){
             ans[i+n]=nums[i];
